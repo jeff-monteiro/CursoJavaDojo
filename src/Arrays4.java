6facf1cab76c4e93794c40fd908fcc72591e2592
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2021-06-10 19:56:31 -0300

    Arrays4

--- src/Arrays4.java
+++ src/Arrays4.java
@@ -1,5 +1,8 @@
 public class Arrays4 {
     public static void main(String[] args){
-
+        String[] nomes = {"Jefferson", "Karoliny", "Joelma", "Giovanni"};
+        for(String test : nomes){
+            System.out.println(test);
+        }
     }
 }
