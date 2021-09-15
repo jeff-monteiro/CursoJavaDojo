e9793cd4db7cca87195be1c9b539a9bfa158a072
Jefferson Monteiro <81328013+jeff-monteiro@users.noreply.github.com>
2021-09-15 00:25:27 -0300

    Deleting products

--- src/produtos/Autenticacao.java
+++
@@ -1,7 +0,0 @@
-package produtos;
-
-public interface Autenticacao {
-    boolean autenticaCodigo(long codigoDeBarras);
-
-
-}
--- src/produtos/Principal.java
+++
@@ -1,9 +0,0 @@
-package produtos;
-
-public class Principal {
-    public static void main(String[] args) {
-        Produto produto = new Computador();
-
-
-    }
-}
