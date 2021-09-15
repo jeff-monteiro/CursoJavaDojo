a82d77aa57b64bf444e534620e28d73944a5b2fb
Jefferson <jeffmonteiro61@icloud.com>
2021-09-15 12:07:44 -0300

    Switch_DiaUtil

--- src/exercicios/ExercicioControleFluxo2.java
+++ src/exercicios/ExercicioControleFluxo2.java
@@ -1,40 +1,30 @@
 package exercicios;
+
 import java.util.Scanner;
+
 public class ExercicioControleFluxo2 {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         //Crie um switch que dado o valor de 1 a 7
         //considerando 1 domingo imprima se é dia útil ou final de semana
-        System.out.println("Selecione um dia, sabendo que Domingo não é dia útil");
+        System.out.println("Selecione um dia, sabendo que Sábado e Domingo não são dias úteis");
         Scanner selecioneDia = new Scanner(System.in);
         byte dia = selecioneDia.nextByte();
 
-        switch(dia){
+        switch (dia) {
             case 1:
-            if(dia == 1) {
-                System.out.println("Domingo não é dia útil");
-            }
+            case 7:
+                System.out.println("Não é dia útil");
                 break;
             case 2:
-                System.out.println("Segunda é dia útil");
-                break;
             case 3:
-                System.out.println("Terça é dia útil");
-                break;
             case 4:
-                System.out.println("Terça é dia útil");
-                break;
             case 5:
-                System.out.println("Terça é dia útil");
-                break;
             case 6:
-                System.out.println("Terça é dia útil");
-                break;
-            case 7:
-                System.out.println("Terça é dia útil");
+                System.out.println("Dia útil");
                 break;
             default:
-                System.out.println("Selecione um dia");
+                System.out.println("Selecione uma opção!");
 
         }
 
