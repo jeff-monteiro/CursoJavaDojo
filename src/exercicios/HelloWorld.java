ff7977e80de86b6e303eac16d903b3fb60f17b45
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-09 23:21:45 -0300

    chore: keepMotivated

--- src/exercicios/HelloWorld.java
+++ src/exercicios/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Jefferson testando!");
+        System.out.println("I'm gonna learn the Java Language cause I like it!");
     }
 }
