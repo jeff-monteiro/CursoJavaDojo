40e90bbc212f1b88c5bbbb3d64eb4f7ee5433f8a
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-10 22:22:01 -0300

    exercisePrimitiveTypes

--- Notations of the DevDojo Course.md
+++ Notations of the DevDojo Course.md
@@ -9,5 +9,7 @@
  Ex.: double salarioDouble = 2500.01F;
       float salarioFloat = (float)3000.05;
  
- -                                 
+ - String em Java é uma classe, portanto deve ser sempre usada com a primeira letra maiúscula.
+ 
+ -                                  
                          
---
+++ src/academy/devdojo/maratonajava/exercicios/TipoPrimitivosExercicios.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.exercicios;
+
+import java.util.Scanner;
+
+public class TipoPrimitivosExercicios {
+    public static void main(String[] args) {
+
+        String nome, endereco, data;
+        double salario;
+
+        nome = "Jefferson Monteiro de Moura";
+        endereco = "Rua Livreiro Gualter";
+        salario = (double)5000.40;
+        data = "05-04-2022";
+
+        System.out.println("Eu " + nome + " morando no endereço: " + endereco + " confirmo que recebi o salario de: " + salario + " na data de: " + data);
+
+
+
+    }
+}
