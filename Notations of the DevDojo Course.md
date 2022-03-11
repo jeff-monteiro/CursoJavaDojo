231fae94bffad5881ef85abf6e412f3abdcb9064
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-10 21:27:45 -0300

    primitiveTypes&Casting

---
+++ Notations of the DevDojo Course.md
@@ -0,0 +1,13 @@
+      **Notations of the Java Course**
+       
+      *_Tipos Primitivos_* 
+      
+ - int, double, float, char, byte, short, long and boolean.
+ 
+ - casting (Two ways to make the same thing in Java) 
+ 
+ Ex.: double salarioDouble = 2500.01F;
+      float salarioFloat = (float)3000.05;
+ 
+ -                                 
+                         
---
+++ src/academy/devdojo/maratonajava/introduçao/TiposPrimitivos.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.introduçao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, char, long, byte, float, short, boolean
+
+        int age = 10;
+        double salarioDouble = 2000.05;
+        float salarioFloat = (float)2500.01;
+        short idadeShort = 10;
+        byte idadeByte = 11;
+        boolean verdadeiroB = true;
+        boolean falsoB = false;
+        long bigNumber = 200000;
+        char caractere = 87;
+
+
+        System.out.println(age);
+        System.out.println(salarioDouble);
+        System.out.println(salarioFloat);
+        System.out.println(idadeShort);
+        System.out.println(idadeByte);
+        System.out.println(verdadeiroB);
+        System.out.println(falsoB);
+        System.out.println(bigNumber);
+        System.out.println(caractere);
+    }
+}
