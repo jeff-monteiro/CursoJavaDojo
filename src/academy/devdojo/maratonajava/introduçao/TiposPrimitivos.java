116425c68204709aa60d0b0662600183fc068bd6
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-07-07 18:41:05 -0300

    justUpdate

--- src/academy/devdojo/maratonajava/introduçao/TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introduçao/TiposPrimitivos.java
@@ -5,23 +5,23 @@ public class TiposPrimitivos {
         // int, double, char, long, byte, float, short, boolean
 
         int age = 10;
-        double salarioDouble = 2000.05;
-        float salarioFloat = (float)2500.01;
-        short idadeShort = 10;
-        byte idadeByte = 11;
-        boolean verdadeiroB = true;
-        boolean falsoB = false;
+        double wageDouble = 2000.05;
+        float wageFloat = (float)2500.01;
+        short ageShort = 10;
+        byte ageByte = 11;
+        boolean trueB = true;
+        boolean falseB = false;
         long bigNumber = 200000;
         char caractere = 87;
 
 
         System.out.println(age);
-        System.out.println(salarioDouble);
-        System.out.println(salarioFloat);
-        System.out.println(idadeShort);
-        System.out.println(idadeByte);
-        System.out.println(verdadeiroB);
-        System.out.println(falsoB);
+        System.out.println(wageDouble);
+        System.out.println(wageFloat);
+        System.out.println(ageShort);
+        System.out.println(ageByte);
+        System.out.println(trueB);
+        System.out.println(falseB);
         System.out.println(bigNumber);
         System.out.println(caractere);
     }
